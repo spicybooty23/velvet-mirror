aeb9f36264140df21dad4c2a30725fb53d708932
spicybooty23 <unabomberconsumer@gmail.com>
2025-04-23 03:40:09 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -68,6 +68,66 @@ const questions = {
         { text: "I like switching speeds to tease", next: null },
         { text: "Letting them ride while I take control halfway", next: null }
       ]
+    },
+    {
+      text: "Have you received oral sex before?",
+      options: [
+        { text: "Yes", next: 9 },
+        { text: "No", next: 13 }
+      ]
+    },
+    {
+      text: "How do you like oral best?",
+      options: [
+        { text: "Slow, teasing, with eye contact", next: 10 },
+        { text: "Deepthroat until she gags", next: 10 },
+        { text: "Licks and suction on the tip", next: 10 },
+        { text: "Sloppy and messy with spit", next: 10 }
+      ]
+    },
+    {
+      text: "What kind of tongue work makes you melt?",
+      options: [
+        { text: "Flat licks up the shaft", next: 11 },
+        { text: "Circle around the head", next: 11 },
+        { text: "Tip of the tongue flicking my slit", next: 11 },
+        { text: "A combo while stroking me", next: 11 }
+      ]
+    },
+    {
+      text: "Where do you usually cum during oral?",
+      options: [
+        { text: "In her mouth", next: 14 },
+        { text: "On her tongue", next: 14 },
+        { text: "On her face", next: 14 },
+        { text: "I pull out and stroke onto her", next: 14 }
+      ]
+    },
+    {
+      text: "What’s the hottest oral moment you remember?",
+      options: [
+        { text: "She gagged but kept going", next: null },
+        { text: "She looked up at me while I came", next: null },
+        { text: "It was so wet and messy I lost control", next: null },
+        { text: "She sucked me dry then kissed me", next: null }
+      ]
+    },
+    {
+      text: "Do you fantasize about receiving oral?",
+      options: [
+        { text: "Yes, often", next: 14 },
+        { text: "Sometimes", next: 14 },
+        { text: "Rarely", next: 14 }
+      ]
+    },
+    {
+      text: "What would you want them to do first?",
+      options: [
+        { text: "Kiss the head softly", next: 11 },
+        { text: "Take it in fast", next: 11 },
+        { text: "Lick and stroke together", next: 11 },
+        { text: "Moan while they suck", next: 11 }
+      ]
     }
   ],
   female: []
